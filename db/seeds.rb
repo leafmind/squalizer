1ab3faf6cb610dd92b6867b89ba9b778ca8c9962
Evgeniy <leafmind@gmail.com>
2016-12-29 14:38:04 +0300

    add sandbox seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(provider: 'square', token: 'sandbox-sq0atb-MKh5RyMa3ASgbDrR16gDXw', sandbox: true, state: :ready)
